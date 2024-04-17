940ccee360bbe6b9b7337d08dbd191c1ec70604f
Lucas Rios <lucas.rios@aluno.senai.br>
2024-04-17 08:18:55 -0300

    feat: implements users repository try catch blocks

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import { router } from "./routes/index.routes.js";
 
+//connection();
 config();
 
 const port = process.env.PORT || 5000;
